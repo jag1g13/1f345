2d724ec1c372d6c969baba4085f56796f4caede9
James Graham <j.graham@software.ac.uk>
2020-02-07 10:13:56 +0000

    Add user time unit field

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from constance import settings
 
 
 class User(AbstractUser):
@@ -7,3 +10,13 @@ class User(AbstractUser):
 
     See https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
     """
+
+    #: User's prefered units of time measurement
+    time_units = models.CharField(max_length=15,
+                                  blank=True, null=True)
+
+    def get_time_units(self) -> str:
+        """
+        Get the time units used by this user.
+        """
+        return self.time_units or settings['DEFAULT_TIME_UNIT']
